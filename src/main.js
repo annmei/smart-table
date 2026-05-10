cb1da674d112a74c093aa107bf3830875ef4ee2c
Anna Gaidina <anyachum@icloud.com>
2026-05-10 13:31:30 +0300

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './fonts/ys-display/fonts.css';
 import './style.css';
 
-// import { data as sourceData } from "./data/dataset_1.js";
+import { data as sourceData } from "./data/dataset_1.js";
 
 import { initData } from "./data.js";
 import { processFormData } from "./lib/utils.js";
